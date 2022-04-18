f08049358d0cc61e6769f24ac61720708f04d3f6
Varneon <me@varneon.com>
2022-04-18 05:40:10 -0700

    Initial file commit

---
+++ Packages/com.varneon.webrequestwrapper/Editor/WebRequestHandler.cs
@@ -0,0 +1,109 @@
+﻿
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Varneon.WebRequest
+{
+    /// <summary>
+    /// Core web request handler
+    /// </summary>
+    internal class WebRequestHandler
+    {
+        private static WebRequestHandler instance = null;
+
+        internal static WebRequestHandler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new WebRequestHandler();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Enum flag for indicating the type of data contained in a WebRequestResponse
+        /// </summary>
+        [Flags]
+        internal enum ReturnValue
+        {
+            None = 0,
+            Text = 1,
+            Data = 2
+        }
+
+        /// <summary>
+        /// Response struct for data returned from web request
+        /// </summary>
+        internal struct Response
+        {
+            /// <summary>
+            /// A floating-point value between 0.0 and 1.0, indicating the progress of downloading body data from the server.
+            /// </summary>
+            internal float DownloadProgress;
+
+            /// <summary>
+            /// The numeric HTTP response code returned by the server, such as 200, 404 or 500.
+            /// </summary>
+            internal long ResponseCode;
+
+            /// <summary>
+            /// What data does the current response hold
+            /// </summary>
+            internal ReturnValue DownloadHandlerData;
+
+            /// <summary>
+            /// The raw bytes downloaded from the remote server, or null.
+            /// </summary>
+            internal byte[] Data;
+
+            /// <summary>
+            /// The bytes from data interpreted as a UTF8 string.
+            /// </summary>
+            internal string Text;
+        }
+
+        /// <summary>
+        /// Standard Unity Web Request Enumerator
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>WebRequestResponse</returns>
+        internal IEnumerator Request(string url, ReturnValue returnValues = ReturnValue.None)
+        {
+            Response response = new Response()
+            {
+                DownloadHandlerData = returnValues
+            };
+
+            using (UnityWebRequest request = UnityWebRequest.Get(url))
+            {
+                request.SendWebRequest();
+
+                while (!request.isDone)
+                {
+                    response.DownloadProgress = request.downloadProgress;
+
+                    yield return response;
+                }
+
+                response.ResponseCode = request.responseCode;
+
+                if (request.isHttpError || request.isNetworkError)
+                {
+                    Debug.LogError(request.error);
+                }
+                else
+                {
+                    if (returnValues.HasFlag(ReturnValue.Data)) { response.Data = request.downloadHandler.data; }
+                    if (returnValues.HasFlag(ReturnValue.Text)) { response.Text = request.downloadHandler.text; }
+                }
+            }
+
+            yield return response;
+        }
+    }
+}
---
+++ Packages/com.varneon.webrequestwrapper/Editor/WebRequestUtility.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+
+namespace Varneon.WebRequest
+{
+    /// <summary>
+    /// Web request utility for handling any standard web requests
+    /// </summary>
+    internal static class WebRequestUtility
+    {
+        /// <summary>
+        /// Standard GET UnityWebRequest
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="returnValues">What data from the download handler should be returned in the response</param>
+        /// <returns>WebRequest Response</returns>
+        internal static WebRequestHandler.Response Get(string url, WebRequestHandler.ReturnValue returnValues = WebRequestHandler.ReturnValue.None, bool displayProgressBar = true)
+        {
+            IEnumerator request = WebRequestHandler.Instance.Request(url, returnValues);
+
+            while (request.MoveNext())
+            {
+                if (displayProgressBar)
+                {
+                    EditorUtility.DisplayProgressBar("Web Request", url, ((WebRequestHandler.Response)request.Current).DownloadProgress);
+                }
+            }
+
+            if (displayProgressBar)
+            {
+                EditorUtility.ClearProgressBar();
+            }
+
+            WebRequestHandler.Response response = (WebRequestHandler.Response)request.Current;
+
+            return response;
+        }
+
+        internal static FileDownloadAndSaveResponse DownloadAndSaveFile(string url, string savePath)
+        {
+            FileDownloadAndSaveResponse fileResponse = new FileDownloadAndSaveResponse();
+
+            WebRequestHandler.Response response = Get(url, WebRequestHandler.ReturnValue.Data);
+
+            if (response.ResponseCode != 200)
+            {
+                fileResponse.Error = $"Web Request Error: {response.ResponseCode}";
+
+                return fileResponse;
+            }
+
+            try
+            {
+                File.WriteAllBytes(savePath, response.Data);
+
+                fileResponse.Success = true;
+            }
+            catch (Exception e)
+            {
+                fileResponse.Error = e.Message;
+            }
+
+            return fileResponse;
+        }
+
+        internal struct FileDownloadAndSaveResponse
+        {
+            internal bool Success;
+
+            internal string Error;
+        }
+    }
+}
